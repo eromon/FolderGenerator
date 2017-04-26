e2262ce4ba29891c25ecd19e8514bff622a2077e
eromon <e_oboh@encs.concordia.ca>
2017-04-26 14:17:02 -0400

    Update folder_motor.py

--- folder_motor.py
+++ folder_motor.py
@@ -10,24 +10,27 @@ import pandas as pd
 
 
 
-xl = pd.ExcelFile('dl-auto.xlsx')
-names = xl.sheet_names
+xl = pd.ExcelFile('file_name.xlsx')
+names = xl.sheet_names  #Here we create a list of the sheet_names we intend to access
 names = names[1:]
 #print(names)
-rootPath = "C:\\Users\\Eromon D Great\\.spyder-py3\\motor\\"
-views = ["front_view", "rear_view","side_view"]
-years =[]
+rootPath = "C:\\Users\\File_Path"
+object_views = ["front_view", "rear_view","side_view"]
+#years =[]
 for i,brand in enumerate(names):
-    df = pd.read_excel("dl-auto.xlsx", sheetname=brand)
+    df = pd.read_excel("file_name.xlsx", sheetname=brand)
     years = df["model_year"]
     #df.loc["model_year"]
     years = years.astype(str)
     #print(years,brand)
     os.makedirs(os.path.join(rootPath,brand),exist_ok=True)
     brand = brand.strip()
+    
     for i,year in enumerate(years):
         os.makedirs(os.path.join(rootPath,brand,year),exist_ok=True)
+        
         for s,model in enumerate(df.model_name[years== year].astype(str)):
             os.makedirs(os.path.join(rootPath,brand,year,model),exist_ok=True)
-            for view in views:
+            
+            for view in object_views:
                 os.makedirs(os.path.join(rootPath,brand,year,model,view),exist_ok=True)
